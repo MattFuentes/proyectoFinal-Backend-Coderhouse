ed851e3864481b334028f496cbf3bbd0b9bffe51
Matias Fuentes <88908923+MattFuentes@users.noreply.github.com>
2022-10-10 23:39:12 -0300

    1.1.0

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const routersProductos = require('../routers/routersproductos')
+const routersCarrito = require('../routers/routerscarrito')
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(`public`))
+
+
+app.use('/productos', routersProductos)
+app.use('/carrito', routersCarrito)
+
+
+const conectedServer = app.listen(PORT, () => {
+    console.log(`Server on port: ${PORT}`)
+})
+
+conectedServer.on('error', (error) => {
+    console.log('error:', error)
+})
+
